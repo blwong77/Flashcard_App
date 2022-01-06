2826ed55482aef3c8bd84eaddf7b2fa21dc5a39f
Bradley Wong <blwong77@gmail.com>
2022-01-06 14:40:05 -0800

    changed loading string to 'loading viewdeck...'

--- src/Layout/Deck/ViewDeck.js
+++ src/Layout/Deck/ViewDeck.js
@@ -1,20 +1,58 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { readDeck } from "../../utils/api";
 import Crumb from "../Common/Crumb";
+import DeleteButton from "../Common/DeleteButton";
+import Card from "./Card";
 
 export default function ViewDeck({ decks }) {
   const { deckId } = useParams();
-  const currentDeck = decks.find((deck) => deck.id === Number(deckId));
-  
-  if (currentDeck) {
+  const [currentDeck, setCurrentDeck] = useState([]);
+
+  useEffect(() => {
+    const aborter = new AbortController();
+    readDeck(deckId, aborter.signal).then(setCurrentDeck);
+  }, [deckId]);
+
+  if (currentDeck.id) {
     return (
-      <nav aria-label="breadcrumb">
-        <ol className="breadcrumb">
-          <Crumb destination={"/"} name={"Home"} />
-          <Crumb name={currentDeck.name} isActive={true} />
-        </ol>
-      </nav>
+      <>
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            <Crumb destination={"/"} name={"Home"} />
+            <Crumb name={currentDeck.name} isActive={true} />
+          </ol>
+        </nav>
+        <div>
+          <h4>{currentDeck.name}</h4>
+          <p>{currentDeck.description}</p>
+          <Link to={`/decks/${deckId}/edit`}>
+            <button type="btn" className="btn btn-secondary">
+              Edit
+            </button>
+          </Link>
+
+          <Link to={`/decks/${deckId}/study`}>
+            <button type="btn" className="btn btn-primary">
+              Study
+            </button>
+          </Link>
+
+          <button type="btn" className="btn btn-primary">
+            +Add Cards
+          </button>
+          <DeleteButton deckId={deckId} />
+        </div>
+        <div>
+          <h3>Cards</h3>
+          <div className="list-group">
+            {currentDeck.cards.map((card) => (
+              <Card card={card} deckId={deckId} key={card.id} />
+            ))}
+          </div>
+        </div>
+      </>
     );
   }
-  return <p>Loading...</p>;
+  return <p>Loading ViewDeck...</p>;
 }
